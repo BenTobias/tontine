7765d18ddb953a021db4328a1ea021e9fa50cc8f
edgarw19 <edgarallenpoed@gmail.com>
2014-07-29 12:28:57 -0700

    Working template

--- tontine/routes/index.js
+++ tontine/routes/index.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var User = require('../models/user');
-var Task = require('../models/task');
-/* GET home page. */
 
 
 
@@ -53,67 +51,6 @@ router.get('/profile', isLoggedIn, function(req, res) {
 });
 
 
-router.get('/update', isLoggedIn, function(req, res) {
-  res.render('update.ejs', {
-      user : req.user // get the user out of session and pass to template
-    });
-});
-
-router.post('/update/:id', function(req, res) {
-  var category_fields = req.body.increment;
-
-  console.log("The number of fields is " + category_fields);
-  var category_holder = [];
-  console.log(category_holder);
-  for (var i = 0; i < category_fields; i++) {
-    var task = new Task({
-    taskname : req.body['category'+i+'name'],
-    taskduration : req.body['category'+i+'priority'],
-    taskpriority : req.body['category'+i+'duration']
-    });
-    if (task) {
-      category_holder.push(task);
-    }
-  }
-
-  User.findById(req.params.id,function(err, userup){
-    if (!userup)
-      return next(new Error("Couldn't load user"));
-    else {
-      userup.categories = category_holder;
-      userup.save(function(err) {
-        if (err)
-          console.log('error on update');
-        else
-          console.log('successful update');
-      });
-    }
-  });
-  res.redirect('/profile');
-
-
-});
-
-
-router.get('/search', function(req, res) {      
-  res.render('search', {});
-});
-
-router.post('/search', function(req, res) {
-  var university = req.body.university.toLowerCase();
-  var categorysearch = req.body.categorysearch;
-  if (categorysearch) {
-    var categorysearch = categorysearch.toLowerCase();
-    User.find({categories: {$regex: new RegExp("^" + categorysearch)}, education_key : { $regex: new RegExp("^" + university)}}, function(err, matching_users) {
-      res.render('searchresults', {user_array: matching_users, school : req.body.university, categorysearch: req.body.categorysearch});
-    });
-  }
-  else {
-    User.find({education_key : { $regex: new RegExp("^" + university)}}, function(err, matching_users){
-      res.render('searchresults', {user_array : matching_users, school : req.body.university});
-    });
-  }
-});
 
 
 
